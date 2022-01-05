4358bfa074a86b1bf660c4b90d121e9a913915df
= <till.kemper@yahoo.de>
2022-01-05 04:58:38 +0100

    added loading indicator

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,19 @@
 import '../styles/globals.css'
 import Head from 'next/head'
+import NProgress from 'nprogress';
+import "nprogress/nprogress.css";
+import Router from 'next/router';
+
+NProgress.configure({
+  minimum: 0.3,
+  easing: 'ease',
+  speed: 400,
+  showSpinner: false,
+});
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 function MyApp({ Component, pageProps }) {
   return <>
