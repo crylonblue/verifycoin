8e9472858a89723103e69da8fbd7cfd3928cdd6c
= <till.kemper@yahoo.de>
2022-01-23 01:19:38 +0100

    added minimum-scale

--- pages/_app.js
+++ pages/_app.js
@@ -18,7 +18,7 @@ Router.events.on('routeChangeError', () => NProgress.done());
 function MyApp({ Component, pageProps }) {
   return <>
     <Head>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"></meta>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=0"></meta>
     </Head>
     <Component {...pageProps} />
   </>
