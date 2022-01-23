d4ed164a21900c1264ec9a56cb38553d26bb024d
= <till.kemper@yahoo.de>
2022-01-23 01:07:17 +0100

    added burger menu

--- components/navigation.component.js
+++ components/navigation.component.js
@@ -1,9 +1,78 @@
 import styles from '../styles/Navigation.module.css'
 import Link from 'next/link'
 import Image from 'next/image'
-
+import { useState } from 'react'
+import { motion } from 'framer-motion'
 
 export default function Navigation() {
+    const [burgerOpen, setBurgerState] = useState(false)
+    
+    const topVariants = {
+        closed: {
+            rotate: 0,
+            translateY: 0
+        },
+        opened: {
+            rotate: 45,
+            translateY: 4
+        }
+    }
+
+    const botVariants = {
+        closed: {
+            rotate: 0,
+            translateY: 0
+        },
+        opened: {
+            rotate: -45,
+            translateY: -4
+        }
+    }
+
+    const mobileMenuVariants = {
+        closed: {
+            translateX: "100%",
+            opacity: 0,
+            transition: {
+                delay: .4,
+                duration: .5,
+            }
+        },
+        opened: {
+            translateX: 0,
+            opacity: 1,
+            transition: {
+                duration: .5,
+                staggerChildren: .3,
+            }
+        }
+    }
+
+    const textVariants = {
+        closed: {
+            translateX: 100,
+            opacity: 0
+        },
+        opened: {
+            translateX: 0,
+            opacity: 1,
+            transition: {
+                delay: .5
+            }
+        }
+    }
+
+    const navItems = [
+        {
+            text: "explore",
+            href: "/explore"
+        },
+        {
+            text: "docs",
+            href: "https://docs.verifycoin.io/"
+        }
+    ]
+
     return <nav className={styles.navigation}>
         <div className={styles.logo}>
             <Link href="/">
@@ -11,8 +80,24 @@ export default function Navigation() {
             </Link>
         </div>
         <ul className={styles.navigationList}>
-            <li><Link href="/explore">explore</Link></li>
-            <li><Link href="https://docs.verifycoin.io/">docs</Link></li>
+            {
+                navItems.map(({text, href}, i) => {
+                    return <li key={i}><Link href={href}>{text}</Link></li>
+                })
+            }
+            <div className={styles.burgerMenu} onClick={() => {setBurgerState(!burgerOpen)}}>
+                <motion.div animate={ burgerOpen ? "opened" : "closed"} variants={topVariants} />
+                <motion.div animate={ burgerOpen ? "opened" : "closed"} variants={botVariants} />
+            </div>
         </ul>
+        <motion.div className={styles.mobileMenu} variants={mobileMenuVariants} animate={ burgerOpen ? "opened" : "closed"}>
+            <motion.a className={styles.mobileMenuItem} variants={textVariants} animate={ burgerOpen ? "opened" : "closed"} href="/">home</motion.a>
+            {
+                navItems.map(({text, href}, i) => {
+                    return <motion.a className={styles.mobileMenuItem} variants={textVariants} animate={ burgerOpen ? "opened" : "closed"} href={href}>{text}</motion.a>
+                })
+            }
+
+        </motion.div>
     </nav>
 }
--- components/textTeaser.js
+++ components/textTeaser.js
@@ -3,7 +3,7 @@ import { motion } from "framer-motion"
 
 export default function TextTeaser () {
     return <motion.div className={styles.textTeaser} initial={{opacity: 0, y: 20}} whileInView={{opacity: 1, y: 0}} transition={{delay: 0.3, duration: 0.8}} viewport={{ once: true }}>
-        <h2 className={styles.headline}>Providing safety</h2>
-        <p className={styles.subHeadline}>We want to provide people with a fair and fraud free cryptospace. We want to be a source of enlightment and trust.</p>
+        <h2 className={styles.headline}>Better grasp on projects.</h2>
+        <p className={styles.subHeadline}>We summarize new projects and bundle important metrics.</p>
     </motion.div>
 }
