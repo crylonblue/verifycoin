d5b357936436e5a59f09e509340d8bfb0a7c1f00
= <till.kemper@yahoo.de>
2022-01-24 02:06:35 +0100

    ga fix

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,7 @@ import Head from 'next/head'
 import NProgress from 'nprogress';
 import "nprogress/nprogress.css";
 import Router from 'next/router';
-import { useRouter } from 'next/router';
+import TagManager from 'react-gtm-module';
 import { useEffect } from 'react'
 
 NProgress.configure({
@@ -16,16 +16,15 @@ NProgress.configure({
 Router.events.on('routeChangeStart', () => NProgress.start());
 Router.events.on('routeChangeComplete', (url) => {
   NProgress.done()
-  if (typeof window.gtag !== 'undefined') {
-    window.gtag('config', 'GTM-KM58M3J', {
-      page_path: url,
-    });
-  }
 });
 
 Router.events.on('routeChangeError', () => NProgress.done());
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    TagManager.initialize({ gtmId: 'GTM-KM58M3J' });
+  }, []);
+
   return <>
     <Head>
       <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=0"></meta>
--- pages/_document.js
+++ pages/_document.js
@@ -4,17 +4,6 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <script
-        dangerouslySetInnerHTML={{
-          __html: `
-          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-KM58M3J');
-          `
-        }} />
-
         <link rel="icon" href="/favicon.ico" />
         <link href="https://fonts.googleapis.com/css2?family=Spartan:wght@300;400;500;700&display=swap" rel="stylesheet" />
       </Head>
