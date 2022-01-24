ed68c70cbaf3120487d4add191f182aca1deff7f
= <till.kemper@yahoo.de>
2022-01-24 01:34:44 +0100

    ga fix

--- pages/_document.js
+++ pages/_document.js
@@ -4,21 +4,16 @@ export default function Document() {
   return (
     <Html>
       <Head>
-      <script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=GTM-KM58M3J"
-        />
-
         <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'GTM-KM58M3J', { page_path: window.location.pathname });
-            `,
-          }}
-        />
+        dangerouslySetInnerHTML={{
+          __html: `
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-KM58M3J');
+          `
+        }} />
 
         <link rel="icon" href="/favicon.ico" />
         <link href="https://fonts.googleapis.com/css2?family=Spartan:wght@300;400;500;700&display=swap" rel="stylesheet" />
